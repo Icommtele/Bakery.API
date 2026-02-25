0ef831967b8dc2d90c1a929c93732e56cb291d30
Icommtele <srikantht@icommtele.com>
2026-02-25 11:53:32 +0530

    Add files via upload

--- Bakery.API/Program.cs
+++ Bakery.API/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngular",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200")
+            policy.WithOrigins("http://localhost:4200", "https://jeevan-bakery.netlify.app")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
