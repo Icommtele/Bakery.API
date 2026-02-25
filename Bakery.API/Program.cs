b26f0faadd23066bc3f2c8ecc08863b58fc1b125
Icommtele <srikantht@icommtele.com>
2026-02-25 10:06:24 +0530

    Add files via upload

--- Bakery.API/Program.cs
+++ Bakery.API/Program.cs
@@ -45,6 +45,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 //app.Run();
-app.Run("http://0.0.0.0:8080");
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Run($"http://0.0.0.0:{port}");
 
 
